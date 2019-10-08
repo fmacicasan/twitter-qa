dd6a417494ae412208df50924dd034f6b8ff2576
Florin Macicasan <fmacicasan@sdl.com>
2019-10-08 13:59:34 +0200

    Exercise 5 & 6

--- src/test/java/com/camunda/training/ProcessJUnitTest.java
+++ src/test/java/com/camunda/training/ProcessJUnitTest.java
@@ -1,6 +1,7 @@
 package com.camunda.training;
 
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.test.mock.Mocks;
@@ -11,6 +12,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
@@ -31,11 +33,14 @@ public class ProcessJUnitTest {
     public void testHappyPath() {
         // Create a HashMap to put in variables for the process instance
         Map<String, Object> variables = new HashMap<String, Object>();
-        variables.put("approved", true);
         variables.put("content", "Where is my cattt?");
         // Start process with Java API and variables
         ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
         // Make assertions on the process instance
+        assertThat(processInstance).isWaitingAt("CheckTweetTask");
+        List<Task> tasks = taskService().createTaskQuery().list();
+        assertThat(tasks.get(0)).hasCandidateGroup("management");
+        taskService().complete(tasks.get(0).getId(), withVariables("approved", true));
         assertThat(processInstance).isEnded();
     }
 
@@ -45,6 +50,8 @@ public class ProcessJUnitTest {
         Map<String, Object> variables = new HashMap<String, Object>();
         variables.put("approved", false);
         ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("TwitterQAProcess", variables);
+        assertThat(processInstance).isWaitingAt("CheckTweetTask");
+        taskService().complete(task().getId());
         assertThat(processInstance).isEnded();
     }
 
